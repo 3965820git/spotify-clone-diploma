0468cc64b21ec399f93866c8062d8d8e704297ee
denestrambola <tramboladenes@gmail.com>
2026-04-05 13:54:46 +0300

    feat: implement base classes for pagination

---
+++ src/Shared/BuildingBlocks/SpotifyClone.Shared.BuildingBlocks.Application/Pagination/PagedList.cs
@@ -0,0 +1,11 @@
+﻿namespace SpotifyClone.Shared.BuildingBlocks.Application.Pagination;
+
+public class PagedList<T>(IEnumerable<T> items, int count, int page, int pageSize)
+{
+    public IList<T> Items { get; } = items.ToList();
+    public int Page { get; } = page;
+    public int PageSize { get; } = pageSize;
+    public int TotalCount { get; } = count;
+    public bool HasNextPage => Page * PageSize < TotalCount;
+    public bool HasPreviousPage => Page > 1;
+}
---
+++ src/Shared/BuildingBlocks/SpotifyClone.Shared.BuildingBlocks.Infrastructure/Persistence/Extensions/QueryableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpotifyClone.Shared.BuildingBlocks.Application.Pagination;
+
+namespace SpotifyClone.Shared.BuildingBlocks.Infrastructure.Persistence.Extensions;
+
+public static class QueryableExtensions
+{
+    public static async Task<PagedList<T>> ToPagedListAsync<T>(
+        this IQueryable<T> source,
+        int page,
+        int pageSize)
+    {
+        // 1. Спочатку рахуємо загальну кількість (виконує SELECT COUNT)
+        int count = await source.CountAsync();
+
+        // 2. Витягуємо тільки потрібну сторінку (виконує LIMIT / OFFSET)
+        List<T> items = await source
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return new PagedList<T>(items, count, page, pageSize);
+    }
+}
