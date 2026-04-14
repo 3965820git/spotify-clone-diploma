9cc9503bdffdeb086172e64c2b6aebb7b703ab93
al <ami777ret@gmail.com>
2026-04-14 12:46:46 +0300

    Refactor ShuffleToggle: migrate from PlayerStore to usePlayerControls hook

--- frontend/src/features/player/ui/ShuffleToggle.tsx
+++ frontend/src/features/player/ui/ShuffleToggle.tsx
@@ -1,22 +1,21 @@
 'use client'
 
-import { usePlayerStore } from '@/features/player/model/usePlayerStore'
+import { usePlayerControls } from '@/features/player/lib/usePlayerControls'
 import { IconButton } from '@/shared/ui/buttons/IconButton'
 import { ShuffleIcon } from '@/shared/ui/icons/ShuffleIcon'
 
 export function ShuffleToggle() {
-  const shuffle = usePlayerStore((state) => state.shuffle)
-  const toggleShuffle = usePlayerStore((state) => state.toggleShuffle)
+  const { isShuffled, handleToggleShuffle } = usePlayerControls()
 
   return (
     <IconButton
       type="button"
-      onClick={toggleShuffle}
+      onClick={() => { void handleToggleShuffle() }}
       className="h-[28px] w-[28px] text-groov-accent"
     >
       <ShuffleIcon
         className="h-[24px] w-[24px]"
-        isActive={shuffle}
+        isActive={isShuffled}
       />
     </IconButton>
   )
